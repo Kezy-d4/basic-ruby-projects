5518f52b0c6c393ed5aeb453799b6aaa1aaf6134
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-03-09 23:14:28 -0400

    wip: bubble sort

--- bubble-sort/bubble-sort.rb
+++ bubble-sort/bubble-sort.rb
@@ -1,16 +1,33 @@
-to_sort = [4,3,78,2,0,2]
+# integers = [4,3,78,2,0,2]
 # => [0,2,2,3,4,78]
 
-  one_pass = to_sort.each_with_index do |integer, index|
+def bubble_sort(integers)
+  passes_made = 0 # debugging
+
   
-    next_integer = to_sort[index.next]
-  
-    if next_integer
-      if integer > next_integer
-        to_sort[index], to_sort[index.next] = to_sort[index.next], to_sort[index]
+  integers.length.times do 
+    swap_made = false
+
+    integers.each_with_index do |integer, index|
+      next_integer = integers[index.next]
+
+      if next_integer
+        if integer > next_integer
+          integers[index], integers[index.next] = 
+          integers[index.next], integers[index]
+          swap_made = true
+        end
       end
+
     end
-  
+    
+    passes_made += 1 # debugging
+    break unless swap_made
   end
+  
+  p "total passes: #{passes_made}"# debugging
+  integers
+end
 
-
+p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([0,1,2,3,3,4,5])
