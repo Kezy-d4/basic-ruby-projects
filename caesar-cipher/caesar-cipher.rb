07392a5bd3e2e020b47a540d60496b124bdff07c
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-04-15 16:24:00 -0400

    Practice with rubocop

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,20 +1,20 @@
 require 'pry-byebug'
 
-# Loop through the given string and return a new string with each letter 
+# Loop through the given string and return a new string with each letter
 # shifted the specified number of positions forward (right) in the alphabet.
 # Preserve original whitespace, case, and non-letter characters, and
-# wrap back to the beginning of the alphabet (from z-a) when necessary. 
+# wrap back to the beginning of the alphabet (from z-a) when necessary.
 
 # string = "What a string!"
 # forward_shift = 5
 
 # helper methods
 def is_lower_letter?(string)
-  ("a".."z").include?(string)
+  ('a'..'z').include?(string)
 end
 
 def is_upper_letter?(string)
-  ("A".."Z").include?(string)
+  ('A'..'Z').include?(string)
 end
 
 def is_letter?(string)
@@ -22,36 +22,34 @@ def is_letter?(string)
 end
 
 def find_letter_index(letter)
-
   if is_lower_letter?(letter)
-    ("a".."z").find_index(letter)
+    ('a'..'z').find_index(letter)
 
   elsif is_upper_letter?(letter)
-    ("A".."Z").find_index(letter)
+    ('A'..'Z').find_index(letter)
   end
 end
 
 def shift_letter(letter, forward_shift)
   ltr_idx = find_letter_index(letter)
 
-  if is_lower_letter?(letter) 
-    letter = ("a".."z").to_a[(ltr_idx + forward_shift) % 26]
+  if is_lower_letter?(letter)
+    ('a'..'z').to_a[(ltr_idx + forward_shift) % 26]
 
   elsif is_upper_letter?(letter)
-    letter = ("A".."Z").to_a[(ltr_idx + forward_shift) % 26]
+    ('A'..'Z').to_a[(ltr_idx + forward_shift) % 26]
   end
 end
 
 # main method
 def caesar_cipher
-  puts "Enter a message to be encrypted."
+  puts 'Enter a message to be encrypted.'
   string = gets.chomp
 
-  puts "Enter the forward shift factor."
+  puts 'Enter the forward shift factor.'
   forward_shift = gets.chomp.to_i
 
-  ciphered = string.split("").map do |character|
-
+  ciphered = string.split('').map do |character|
     if is_letter?(character)
       shift_letter(character, forward_shift)
 
@@ -60,14 +58,8 @@ def caesar_cipher
     end
   end
 
-  puts "Your message has been encrypted according to your forward shift factor."
+  puts 'Your message has been encrypted according to your forward shift factor.'
   ciphered.join
-end 
+end
 
 p caesar_cipher
-
-
-
-
-
-
